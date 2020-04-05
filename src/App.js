a3b03fda35db5ad64144a43de85a7d491c4f5f09
Rick Brown <ricky.brown.00@gmail.com>
2020-04-05 23:17:08 +0100

    lays out structure, and recieves data from REST api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Cards, Chart, CountryPicker } from './components'
+import CSS from './App.module.css'
+import { fetchData } from './components/API'
 
 class App extends Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const data = await fetchData()
+    this.setState({ data })
+  }
+
   render() {
+    const { data } = this.state
     return (
-      <div>
+      <div className={CSS.container}>
         <h1>App</h1>
+        <Cards data={data} />
+        <Chart />
+        <CountryPicker />
       </div>
     )
   }
